b9d6c4c70da85a869100a90153c5e4d3dc6b4141
steven0129 <h3584935@gmail.com>
2018-05-26 01:21:29 +0800

    make prototype in main.py and WhiteSnake/Loader.py

--- WhiteSnake/Loader.py
+++ WhiteSnake/Loader.py
@@ -3,11 +3,12 @@ import os
 import csv
 
 class Dataset(data.Dataset):
-    def __init__(self):
+    def __init__(self, ratio=1):
         self.data = list(csv.reader(open(f'{os.path.dirname(os.path.abspath(__file__))}/white-snake-preprocess.csv')))
+        self.data = self.data[:int(len(self.data) * ratio)]
 
-    def __getitem__(self):
-        return self.data[index]
+    def __getitem__(self, index):
+        return (self.data[index], self.data[index + 1])
 
     def __len__(self):
-        return len(self.data)
+        return len(self.data) - 1
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from WhiteSnake.Loader import Dataset
 from utils.Visualization import CustomVisdom
 from config import Env
+from tqdm import tqdm
+import torch
+import torch.utils.data as Data
 
 options = Env()
 
@@ -15,6 +18,30 @@ def train(**kwargs):
         if not key.startswith('__'): configSummary += str(key) + '=' + str(value) + '<br>'
     
     vis.text('config', f'{configSummary}')
+
+    whiteSnake = Dataset(ratio=options.ratio)
+    print(f'收錄{len(whiteSnake)}個pair')
+    print('將每對pair存入X和Y中')
+
+    X = [0] * len(whiteSnake)
+    Y = [0] * len(whiteSnake)
+
+    for i, (x, y) in enumerate(tqdm(whiteSnake)):
+        X[i] = x
+        Y[i] = y
+        vis.text('progress', f'目前資料輸入進度: {i + 1}/{len(whiteSnake)}')
+
+    X = torch.Tensor(X).long()
+    Y = torch.Tensor(Y).long()
+
+    dataset = Data.TensorDataset(data_tensor=X, target_tensor=Y)
+    loader = Data.DataLoader(dataset=dataset, batch_size=options.batch_size, shuffle=options.shuffle, drop_last=True, num_workers=options.core)
+
+    for epoch in tqdm(range(options.epochs)):
+        for index, (batchX, batchY) in enumerate(tqdm(loader)):
+            tqdm.write(str(batchX))
+            tqdm.write(str(batchY))            
+            vis.text('progress', f'目前迭代進度:<br>epochs={epoch + 1}<br>batch={index + 1}')
 
 if __name__ == '__main__':
     import fire
